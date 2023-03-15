2241b696526d3d84c7546d6232bc9d4b859f3af2
Natan Oliveira Santos <71585908+natanOds@users.noreply.github.com>
2023-03-15 20:13:01 -0300

    initialize project

---
+++ src/main/java/com/exquisitecode/apiforms/ApiformsApplication.java
@@ -0,0 +1,13 @@
+package com.exquisitecode.apiforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiformsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exquisitecode/apiforms/ApiformsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exquisitecode.apiforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

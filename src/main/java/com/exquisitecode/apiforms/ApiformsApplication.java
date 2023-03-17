634be6b630cfa3c608e13cbf162b74d9d83693c1
Natan Oliveira Santos <71585908+natanOds@users.noreply.github.com>
2023-03-17 00:15:49 -0300

    connecting to MongoDB with Repository and Service

--- src/main/java/com/exquisitecode/apiforms/ApiformsApplication.java
+++ src/main/java/com/exquisitecode/apiforms/ApiformsApplication.java
@@ -1,6 +1,7 @@
 package com.exquisitecode.apiforms;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/exquisitecode/apiforms/resources/UserResource.java
+++ src/main/java/com/exquisitecode/apiforms/resources/UserResource.java
@@ -1,27 +1,27 @@
 package com.exquisitecode.apiforms.resources;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exquisitecode.apiforms.domain.User;
+import com.exquisitecode.apiforms.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User natan = new User("1", "natan oliveira", "natanods@gmail.com");
-		User kleber = new User("2", "kleber henrique", "kleberh@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(natan, kleber));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
